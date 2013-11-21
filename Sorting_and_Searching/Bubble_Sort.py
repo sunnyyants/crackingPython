6d9036d554f162c362335907a870509653415983
Tingshen Yan <sunnyyants@gmail.com>
2013-11-21 00:15:59 -0500

    Python Solution of Sorting and Searching part

--- Sorting_and_Searching/Bubble_Sort.py
+++ Sorting_and_Searching/Bubble_Sort.py
@@ -5,10 +5,10 @@ __author__ = 'SunnyYan'
 def BubbleSort(array):
     for i in range(0,len(array) - 1):
         for j in range(0,len(array) - i - 1):
-            if(array[i] < array[j]):
-                temp = array[i]
-                array[i] = array[j]
-                array[j] = temp
+            if(array[j] > array[j + 1]):
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
 
     return array
 
